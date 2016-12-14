b9f26c42c8f08995bdd6b75e14324b6c92d340f0
Forrest Hopkins <fingerheroes@gmail.com>
2016-12-13 22:34:44 -0700

    Created basic GUI

--- ui/rootwindow.py
+++ ui/rootwindow.py
@@ -9,8 +9,30 @@ class RootWindow():
         self.centralwidget.setObjectName("CentralWidget")
 
         self.exitbtn = QtWidgets.QPushButton(self.centralwidget)
-        self.exitbtn.setGeometry(QtCore.QRect(20, 180, 70, 40))
+        self.exitbtn.setGeometry(QtCore.QRect(20, 320, 70, 40))
+        self.exitbtn.setText("Exit")
         self.exitbtn.setObjectName("ExitButton")
+
+        self.pullbtn = QtWidgets.QPushButton(self.centralwidget)
+        self.pullbtn.setGeometry(QtCore.QRect(310, 320, 70, 40))
+        self.pullbtn.setText("Pull")
+        self.pullbtn.setObjectName("PullButton")
+
+        self.pushbtn = QtWidgets.QPushButton(self.centralwidget)
+        self.pushbtn.setGeometry(QtCore.QRect(230, 320, 70, 40))
+        self.pushbtn.setText("Push")
+        self.pushbtn.setObjectName("PushButton")
+
+        self.console = QtWidgets.QTextEdit(self.centralwidget)
+        self.console.setGeometry(QtCore.QRect(20, 20, 360, 280))
+        self.console.setReadOnly(True)
+        self.console.setText("This is the output console")
+        self.console.setObjectName("Console")
+
+        self.divider = QtWidgets.QFrame(self.centralwidget)
+        self.divider.setGeometry(QtCore.QRect(20, 300, 360, 20))
+        self.divider.setFrameShape(QtWidgets.QFrame.HLine)
+        self.divider.setObjectName("Divider")
 
         self.menubar = QtWidgets.QMenuBar(window)
         self.menubar.setEnabled(True)
@@ -18,12 +40,17 @@ class RootWindow():
         self.menubar.setObjectName("MenuBar")
 
         self.filemenu = QtWidgets.QMenu(self.menubar)
+        self.filemenu.setTitle("File")
         self.filemenu.setObjectName("FileMenu")
 
         self.savebtn = QtWidgets.QAction(window)
+        self.savebtn.setText("Save...")
         self.savebtn.setObjectName("SaveButton")
 
         self.filemenu.addAction(self.savebtn)
         self.menubar.addAction(self.filemenu.menuAction())
 
+        window.setWindowTitle("Root Window")
+
+        window.setCentralWidget(self.centralwidget)
         window.setMenuBar(self.menubar)
